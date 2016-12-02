0afc5baa2a0fb242f436a30c57be80dfb85950ee
BerliozLeChat <neoflash93@gmail.com>
2016-12-02 14:44:10 +0100

    Ajout de tests

--- src/main/java/org/eclipse/emf/common/util/URITest.java
+++ src/main/java/org/eclipse/emf/common/util/URITest.java
@@ -1,429 +1,96 @@
 package org.eclipse.emf.common.util;
 
+import org.junit.*;
+
 public class URITest {
 
-    @org.junit.Before
-    public void setUp() throws Exception {
+    @Before
+    public void setUp() {
 
     }
 
-    @org.junit.After
-    public void tearDown() throws Exception {
+    @After
+    public void tearDown() {
 
     }
 
-    @org.junit.Test
-    public void testCreateGenericURI() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateHierarchicalURI() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateHierarchicalURI1() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateHierarchicalURI2() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateURI() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateURI1() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateURI2() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateDeviceURI() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateURIWithCache() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreateFileURI() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreatePlatformResourceURI() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreatePlatformResourceURI1() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testCreatePlatformPluginURI() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidScheme() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidOpaquePart() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidAuthority() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidArchiveAuthority() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidJarAuthority() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidDevice() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidSegment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidSegments() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidQuery() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testValidFragment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsRelative() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsHierarchical() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasAuthority() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasOpaquePart() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasDevice() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasPath() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasAbsolutePath() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasRelativePath() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasEmptyPath() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasQuery() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasFragment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsCurrentDocumentReference() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsEmpty() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsFile() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsPlatform() throws Exception {
-
+    /*
+     * Test de la méthode validScheme
+     */
+    @Test
+    public void testValidScheme1() {
+        Assert.assertTrue(URI.validScheme("test"));
     }
 
-    @org.junit.Test
-    public void testIsPlatformResource() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsPlatformPlugin() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsArchive() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testIsArchiveScheme() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testScheme() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testOpaquePart() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testAuthority() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testUserInfo() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHost() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testPort() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testDevice() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testSegments() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testSegmentsList() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testSegmentCount() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testSegment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testLastSegment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testPath() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testDevicePath() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testQuery() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testAppendQuery() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testTrimQuery() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testFragment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testAppendFragment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testTrimFragment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testResolve() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testResolve1() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testDeresolve() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testDeresolve1() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testToFileString() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testToPlatformString() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testAppendSegment() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testAppendSegments() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testTrimSegments() throws Exception {
-
-    }
-
-    @org.junit.Test
-    public void testHasTrailingPathSeparator() throws Exception {
-
+    @Test
+    public void testValidScheme2() {
+        Assert.assertTrue(URI.validScheme(null));
     }
-
-    @org.junit.Test
-    public void testFileExtension() throws Exception {
 
+    @Test
+    public void testValidScheme3() {
+        Assert.assertFalse(URI.validScheme("?"));
     }
 
-    @org.junit.Test
-    public void testAppendFileExtension() throws Exception {
-
+    @Test
+    public void testValidScheme4() {
+        Assert.assertFalse(URI.validScheme("/"));
     }
-
-    @org.junit.Test
-    public void testTrimFileExtension() throws Exception {
 
+    @Test
+    public void testValidScheme5() {
+        Assert.assertFalse(URI.validScheme("#"));
     }
-
-    @org.junit.Test
-    public void testIsPrefix() throws Exception {
-
+    
+    /*
+     *
+     */
+        /*
+     * Test de la méthode validSegment
+     */
+    @Test
+    public void testValidSegment1() {
+        Assert.assertTrue(URI.validSegment("test"));
     }
 
-    @org.junit.Test
-    public void testReplacePrefix() throws Exception {
-
+    @Test
+    public void testValidSegment2() {
+        Assert.assertFalse(URI.validSegment(null));
     }
 
-    @org.junit.Test
-    public void testEncodeOpaquePart() throws Exception {
-
+    @Test
+    public void testValidSegment3() {
+        Assert.assertFalse(URI.validSegment("?"));
     }
-
-    @org.junit.Test
-    public void testEncodeAuthority() throws Exception {
 
+    @Test
+    public void testValidSegment4() {
+        Assert.assertFalse(URI.validSegment("/"));
     }
 
-    @org.junit.Test
-    public void testEncodeSegment() throws Exception {
-
+    @Test
+    public void testValidSegment5() {
+        Assert.assertFalse(URI.validSegment("#"));
     }
-
-    @org.junit.Test
-    public void testEncodeQuery() throws Exception {
 
+    /*
+     * Test de validSegment
+     */
+    @Test
+    public void testValidSegments1() {
+        String[] list = {"","?"};
+        Assert.assertFalse(URI.validSegments(list));
     }
 
-    @org.junit.Test
-    public void testEncodeFragment() throws Exception {
-
+    @Test
+    public void testValidSegments2() {
+        String[] list = {"","ok"};
+        Assert.assertTrue(URI.validSegments(list));
     }
-
-    @org.junit.Test
-    public void testDecode() throws Exception {
 
+    @Test
+    public void testValidSegments3() {
+        String[] list = {};
+        Assert.assertTrue(URI.validSegments(list));
     }
 }
